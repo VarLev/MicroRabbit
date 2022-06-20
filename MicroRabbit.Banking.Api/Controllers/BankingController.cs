b94d64acb34d69d0b210dc360d1d81fb22008807
Lev Feoktistov <levromf@gmail.com>
2022-06-20 19:26:43 +0300

    Partially completed Banking Microservice

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,25 @@
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService=accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+       
+    }
+}
